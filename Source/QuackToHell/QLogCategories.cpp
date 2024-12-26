632cb31ebab1d9885c4d08969860a79a1a3a8fd1
Seohyun Ryu <atz999@ewhain.net>
2024-12-26 20:16:37 +0900

    Initial commit: asset 추가 + 로그 카테고리 설정

---
+++ Source/QuackToHell/QLogCategories.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QLogCategories.h"
+
+DEFINE_LOG_CATEGORY(LogLogic);
+DEFINE_LOG_CATEGORY(LogNPC);
---
+++ Source/QuackToHell/QLogCategories.h
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+DECLARE_LOG_CATEGORY_EXTERN(LogLogic, Log, All);
+DECLARE_LOG_CATEGORY_EXTERN(LogNPC, Log, All);
