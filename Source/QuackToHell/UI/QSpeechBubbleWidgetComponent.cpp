001ca6137dbbd5d7ac750f1acb21d913a0038dc7
YujinJeon <yujinjun@ewhain.net>
2025-01-30 16:51:22 +0900

    [SCRUM-434] feat: 공용인터페이스 추가

---
+++ Source/QuackToHell/UI/QSpeechBubbleWidgetComponent.cpp
@@ -0,0 +1,16 @@
+// Copyright_Team_AriAri
+
+
+#include "UI/QSpeechBubbleWidgetComponent.h"
+
+void UQSpeechBubbleWidgetComponent::TurnOnSpeechBubble()
+{
+}
+
+void UQSpeechBubbleWidgetComponent::TurnOffSpeechBubble()
+{
+}
+
+void UQSpeechBubbleWidgetComponent::UpdateText(FString& Text)
+{
+}
