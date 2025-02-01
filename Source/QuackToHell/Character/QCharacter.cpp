43f98c6a7fff97cd11105f63781f23c0798c5dc5
YujinJeon <yujinjun@ewhain.net>
2025-02-01 23:41:42 +0900

    [SCRUM-440] feat: 이름이 NPC, Player의 머리 위에 뜨도록

--- Source/QuackToHell/Character/QCharacter.cpp
+++ Source/QuackToHell/Character/QCharacter.cpp
@@ -2,21 +2,53 @@
 
 
 #include "Character/QCharacter.h"
+#include "Blueprint/UserWidget.h"
+#include "UObject/SoftObjectPath.h"
+#include "UI/QNameWidget.h"
+#include "Components/WidgetComponent.h"
 
 // Sets default values
 AQCharacter::AQCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	/*이름표 UI 컴포넌트*/
+	this->WidgetComponent= CreateDefaultSubobject<UWidgetComponent>(TEXT("NameWidget"));
+	EWidgetSpace WidgetSpace = EWidgetSpace::Screen;
+	this->WidgetComponent->SetWidgetSpace(WidgetSpace);
+	this->WidgetComponent->SetDrawAtDesiredSize(true);
+	this->WidgetComponent->SetupAttachment(RootComponent);
+	TSubclassOf<UQNameWidget> _NameWidget;
+	//QNameWidget을 상속한 클래스만 담을 수 있도록 강제한다.
+	this->WidgetComponent->SetWidgetClass(_NameWidget);
 }
 
 // Called when the game starts or when spawned
 void AQCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (WidgetComponent)
+	{
+		UUserWidget* UserWidget = WidgetComponent->GetWidget();
+		if (UserWidget)
+		{
+			NameWidget = Cast<UQNameWidget>(UserWidget);
+		}
+	}
 }
+
+const FString& AQCharacter::GetCharacterName() const
+{
+	return CharacterName;	
+}
+
+void AQCharacter::SetCharacterName(FString& Name)
+{
+	CharacterName = Name;
+}
+
+
 
 // Called every frame
 void AQCharacter::Tick(float DeltaTime)
--- Source/QuackToHell/Character/QNPC.cpp
+++ Source/QuackToHell/Character/QNPC.cpp
@@ -3,7 +3,10 @@
 
 #include "Character/QNPC.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/WidgetComponent.h"
+#include "QLogCategories.h"
 #include "Net/UnrealNetwork.h"
+#include "UI/QNameWidget.h"
 
 AQNPC::AQNPC()
 {	/*위치값 세팅*/
@@ -11,6 +14,8 @@ AQNPC::AQNPC()
 	this->GetMesh()->SetRelativeRotation(FRotator(0.f, 0.f, -90.f));
 	/*캡슐 콜라이더 세팅*/
 	this->GetCapsuleComponent()->InitCapsuleSize(50.0f, 60.0f);
+
+
 }
 
 // ---------------------------------------------------------------------------------- //
@@ -24,14 +29,16 @@ void AQNPC::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifet
 	DOREPLIFETIME(AQNPC, bCanFinishConversN2N);
 }
 
-
 void AQNPC::ServerRPCCanCanStartConversN2N_Implementation(const AQNPC* NPC)
 {
-	
 }
+
 void AQNPC::ServerRPCCanCanFinishConversN2N_Implementation(const AQNPC* NPC)
 {
 }
+
+
+
 
 // ---------------------------------------------------------------------------------- //
 
@@ -46,3 +53,18 @@ bool AQNPC::GetCanFinishConversN2N(const AQNPC* NPC)
 	ServerRPCCanCanFinishConversN2N_Implementation(NPC);
 	return bCanFinishConversN2N;
 }
+
+void AQNPC::BeginPlay()
+{
+	Super::BeginPlay();
+	/*이름 세팅*/
+	FString _Name = TEXT("NPC");
+	this->SetCharacterName(_Name);
+	NameToNameWidget();
+}
+
+void AQNPC::NameToNameWidget()
+{
+	Super::NameWidget->SetNameWidgetText(Super::GetCharacterName());
+}
+
