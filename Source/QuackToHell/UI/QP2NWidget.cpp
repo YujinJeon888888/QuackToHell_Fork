8b384a9527e0131b12c02c40ce05ea3e70e3b3a2
YujinJeon <yujinjun@ewhain.net>
2025-02-02 18:38:00 +0900

    feat: P2N UI 생성

--- Source/QuackToHell/UI/QP2NWidget.cpp
+++ Source/QuackToHell/UI/QP2NWidget.cpp
@@ -2,7 +2,14 @@
 
 
 #include "UI/QP2NWidget.h"
+#include "Components/TextBlock.h"
 
-void UQP2NWidget::UpdateText(FString& Text)
+void UQP2NWidget::UpdatePlayerText(FString& Text)
 {
+	PlayerText->SetText(FText::FromString(Text));
+}
+
+void UQP2NWidget::UpdateNPCText(FString& Text)
+{
+	NPCText->SetText(FText::FromString(Text));
 }
