0eca387e7b73d6692259e019130767b84ac120fc
YujinJeon <yujinjun@ewhain.net>
2025-02-03 10:53:31 +0900

    [SCRUM-432] feat: 논리 연결 완료
    구현이 비어있어, 연결 확인하려고 임시 코드들 있음: Todo, 임시 주석 달린 거 후 처리 예정

---
+++ Source/QuackToHell/UI/QSpeechBubbleWidget.h
@@ -0,0 +1,17 @@
+// Copyright_Team_AriAri
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "QSpeechBubbleWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUACKTOHELL_API UQSpeechBubbleWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
